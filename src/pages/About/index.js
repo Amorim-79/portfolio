ed7a2c0c29dc6612dbc5e941da0ef201d255ed07
PH-WebDeveloper <pedrobatutu@gmail.com>
2020-06-01 02:22:40 -0300

    Alteração do estilo nas páginas dos projetos

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FiArrowRight} from 'react-icons/fi'
+import { FiArrowRight } from 'react-icons/fi'
 
 import './styles.css'
 
@@ -7,35 +7,35 @@ import Header from '../Fixed/Header'
 import Footer from '../Fixed/Footer'
 
 export default function About() {
-    return(
+    return (
         <div className="page">
             <React.Fragment>
                 <Header />
                 <main className="container">
                     <div className="box-content">
                         <p className="text-about">
-                        Meu nome é Pedro, sou um estudante de Ciência da computação cursando atualmente o 5º semestre.
-                         Tenho focado meus estudos em desenvolvimento fullstack para web e mobile,
-                         a cada dia buscando por novos conhecimentos e desafios para alcançar meus objetivos como desenvolvedor.
+                            Meu nome é Pedro, sou um estudante de Ciência da computação cursando atualmente o 5º semestre.
+                            Tenho focado meus estudos em desenvolvimento fullstack para web e mobile,
+                            a cada dia buscando por novos conhecimentos e desafios para alcançar meus objetivos como desenvolvedor.
                         </p>
                     </div>
 
                     <div className="box-content" >
                         <p className="group-skills">
-                            <p className="skill">Conhecimentos: <br/>
+                            <p className="skill">Conhecimentos: <br />
                                 HTML5/CSS3 <span className="linha">|</span> Javascript <span className="linha">|</span> Bootstrap
                                 <span className="linha"> |</span> jQuery <span className="linha"> |</span> VueJS <span className="linha">|</span> SQL
                                 <span className="linha">|</span> Gulp <span className="linha">|</span> Webpack <span className="linha"> |</span> MongoDB <span className="linha">|</span> Git / Github <span className="linha">|</span> Jest.</p>
-                            <p className="skill">Principais tecnologias: <br/>
+                            <p className="skill">Principais tecnologias: <br />
                                 ReactJs <span className="linha">|</span> NodeJs <span className="linha">|</span> ReactNativeJs</p>
                         </p>
                     </div>
 
                     <a target="_blank" href="https://drive.google.com/open?id=1Nl6EN8Lf3RDfVanmHMIvuRqMiCT8Jmog" className="button-download">
                         <p className="text-download">
-                        Visualize o currículo
+                            Visualize o currículo
                         </p>
-                        <FiArrowRight size={35} color="#E02041" style={{marginLeft: '15px'}} />
+                        <FiArrowRight size={35} color="#E02041" style={{ marginLeft: '15px' }} />
                     </a>
                 </main>
                 <Footer />
